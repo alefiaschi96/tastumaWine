7b86b9b075e9bafa6d1055fe76ee025a9bda4b15
alefiaschi96 <alessandro.fiaschi96@gmail.com>
2023-04-03 00:59:51 +0200

    ip

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -9,7 +9,7 @@ import { Wine } from './models/wine';
 export class ServiceService {
 
   
-  private apiUrl = 'http://192.168.0.112:3000/api';
+  private apiUrl = 'http://192.168.1.20:3000/api';
 
   constructor(private http: HttpClient) { }
 
